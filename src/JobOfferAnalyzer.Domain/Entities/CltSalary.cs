0d61821e8cb5a5cab85910a5436c3a6bfa44b13d
antonioRenato <antoniorpfs@gmail.com>
2025-04-21 22:52:55 -0300

    Created entities

---
+++ src/JobOfferAnalyzer.Domain/Entities/CltSalary.cs
@@ -0,0 +1,8 @@
+﻿namespace JobOfferAnalyzer.Domain.Entities
+{
+    public class CltSalary
+    {
+        public decimal GrossSalary { get; set; }
+        public decimal MealAllowance { get; set; }
+    }
+}
---
+++ src/JobOfferAnalyzer.Domain/Entities/PjSalary.cs
@@ -0,0 +1,8 @@
+﻿namespace JobOfferAnalyzer.Domain.Entities
+{
+    public class PjSalary
+    {
+        public decimal GrossValue { get; set; }
+        public decimal AccountantFee { get; set; }
+    }
+}
