2a040b98df4ad811d4f60fe34061da3bdc5f98e7
antonioRenato <antoniorpfs@gmail.com>
2025-04-27 20:46:03 -0300

    created calculator for inss and Ir

---
+++ src/JobOfferAnalyzer.Application/Services/CalcInssServices.cs
@@ -0,0 +1,42 @@
+﻿using JobOfferAnalyzer.Application.Interface;
+using System.Runtime.CompilerServices;
+
+namespace JobOfferAnalyzer.Application.Services
+{
+    public class CalcInssServices : ICalcInssServices
+    {
+        private const decimal Ceiling = 7786.02m;
+
+        public decimal ComputeDeductionInss(decimal salary)
+        {
+            decimal salaryToConsider = salary > Ceiling ? Ceiling : salary;
+
+            decimal deduction = 0.0m;
+
+            if (salaryToConsider <= 1412.00m)
+            {
+                deduction += salaryToConsider * 0.075m;
+            }
+            else if (salaryToConsider <= 2666.68m)
+            {
+                deduction += (1412.00m * 0.075m);
+                deduction += (salaryToConsider - 1412.00m) * 0.09m;
+            }
+            else if (salaryToConsider <= 4000.03m)
+            {
+                deduction += (1412.00m * 0.075m);
+                deduction += (2666.68m - 1412.00m) * 0.09m;
+                deduction += (salaryToConsider - 2666.68m) * 0.12m;
+            }
+            else
+            {
+                deduction += (1412.00m * 0.075m);
+                deduction += (2666.68m - 1412.00m) * 0.09m;
+                deduction += (4000.03m - 2666.68m) * 0.12m;
+                deduction += (salaryToConsider - 4000.03m) * 0.14m;
+            }
+
+            return Math.Round(deduction, 2);
+        }
+    }
+}
---
+++ src/JobOfferAnalyzer.Application/Services/CalcIrServices.cs
@@ -0,0 +1,37 @@
+﻿using JobOfferAnalyzer.Application.Interface;
+
+namespace JobOfferAnalyzer.Application.Services
+{
+    public class CalcIrServices : ICalcIrServices
+    {
+        private const decimal FIRST_BRACKET_LIMIT = 2259.20m;
+        private const decimal SECOND_BRACKET_LIMIT = 3000.00m;
+        private const decimal THIRD_BRACKET_LIMIT = 4500.00m;
+        private const decimal FOURTH_BRACKET_LIMIT = 5500.00m;
+
+        private const decimal FIRST_BRACKET_DEDUCTION = 169.44m;
+        private const decimal SECOND_BRACKET_DEDUCTION = 381.44m;
+        private const decimal THIRD_BRACKET_DEDUCTION = 662.69m;
+        private const decimal FOURTH_BRACKET_DEDUCTION = 896.00m;
+
+
+        public decimal ComputeBaseIr(decimal salary, decimal baseInss)
+        {
+            return salary - baseInss;
+        }
+
+        public decimal ComputeDeductionIR(decimal baseIr)
+        {
+            if (baseIr <= FIRST_BRACKET_LIMIT)
+                return 0m;
+            else if (baseIr <= SECOND_BRACKET_LIMIT)
+                return Math.Round((baseIr * 0.075m) - FIRST_BRACKET_DEDUCTION, 2);
+            else if (baseIr <= THIRD_BRACKET_LIMIT)
+                return Math.Round((baseIr * 0.15m) - SECOND_BRACKET_DEDUCTION, 2);
+            else if (baseIr <= FOURTH_BRACKET_LIMIT)
+                return Math.Round((baseIr * 0.225m) - THIRD_BRACKET_DEDUCTION, 2);
+            else
+                return Math.Round((baseIr * 0.275m) - FOURTH_BRACKET_DEDUCTION, 2);
+        }
+    }
+}
