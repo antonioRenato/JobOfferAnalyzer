2dbcd98fbbe53b8f1511c63416af8b7a64fd8a3e
antonioRenato <antoniorpfs@gmail.com>
2025-05-01 21:12:31 -0300

    Created DependencyInjections to make all injections needed

---
+++ src/JobOfferAnalyzer.ConsoleApp/IoC/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using JobOfferAnalyzer.Application.Factory;
+using JobOfferAnalyzer.Application.Interface;
+using JobOfferAnalyzer.Application.Interface.Factory;
+using JobOfferAnalyzer.Application.Interface.Strategy;
+using JobOfferAnalyzer.Application.Interface.UseCase;
+using JobOfferAnalyzer.Application.Services;
+using JobOfferAnalyzer.Application.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobOfferAnalyzer.ConsoleApp.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Services
+            services.AddScoped<ICalcInssServices, CalcInssServices>();
+            services.AddScoped<ICalcIrServices, CalcIrServices>();
+            services.AddScoped<ICalcFgtsServices, CalcFgtsServices>();
+
+            // Strategies
+            services.AddScoped<CltSalaryCalculationStrategy>();
+            services.AddScoped<PjSalaryCalculationStrategy>();
+
+            // Factory
+            services.AddScoped<ISalaryStrategyFactory, SalaryStrategyFactory>();
+
+            // UseCases
+            services.AddScoped<ICalculateSalaryUseCase, CalculateSalaryUseCase>();
+
+            return services;
+        }
+    }
+}
--- src/JobOfferAnalyzer.ConsoleApp/Program.cs
+++ src/JobOfferAnalyzer.ConsoleApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JobOfferAnalyzer.Application.Interface.UseCase;
+using JobOfferAnalyzer.ConsoleApp.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using JobOfferAnalyzer.Domain.Enums;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddApplicationServices();
+
+        var provider = services.BuildServiceProvider();
+
+        var salaryUseCase = provider.GetRequiredService<ICalculateSalaryUseCase>();
+
+        var result = salaryUseCase.Execute(6000, ContractType.CLT);
+        Console.WriteLine($"Salário líquido: {result.NetSalary:C}");
+    }
+}
