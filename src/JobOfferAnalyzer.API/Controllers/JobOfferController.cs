e47befca7ecc22454605a1447e719c24c56671a1
antonioRenato <antoniorpfs@gmail.com>
2025-05-05 22:55:33 -0300

    Created controller JobOffer

---
+++ src/JobOfferAnalyzer.API/Controllers/JobOfferController.cs
@@ -0,0 +1,16 @@
+﻿using JobOfferAnalyzer.Communication.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobOfferAnalyzer.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class JobOfferController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] SalaryCalculationRequest request)
+        {
+            return Ok();
+        }
+    }
+}
